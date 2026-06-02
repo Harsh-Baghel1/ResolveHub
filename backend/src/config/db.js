8fee9608f0aa90dfde547a6286f208f2adf962b8
Harsh-Baghel1 <harshbaghel5720@gmail.com>
2026-06-03 00:35:49 +0530

    feat: add smart complaint assignment, agent management, chat foundation and MongoDB DNS fix

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,12 +1,20 @@
 const mongoose = require("mongoose");
+const dns = require("dns");
+
+dns.setServers([
+  "8.8.8.8",
+  "8.8.4.4",
+]);
 
 const connectDB = async () => {
   try {
     console.log("Connecting to DB...");
+
     await mongoose.connect(process.env.MONGO_URI);
+
     console.log("MongoDB Connected ✅");
   } catch (error) {
-    console.error("DB Error:", error.message);
+    console.error(error);
     process.exit(1);
   }
 };
